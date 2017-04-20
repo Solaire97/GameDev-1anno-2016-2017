5f9591c396bc5d4156ce4f9db497640d345b7300
Utente SoftDev1 <softdev1@comics.local>
2017-04-20 12:21:34 +0200

    Rouge Like

---
+++ Esercitazioni/Survival_Shooter - FPS/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform playerTransform;
+	public float smoothing = 5f;
+	public float sensitivex = 5f;
+
+	private Vector3 _offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (playerTransform != null) 
+		{
+			_offset = transform.position - playerTransform.position;
+		}
+	}
+
+	/*void Rotate()
+	{
+		transform.Rotate(0, Input.GetAxis("Mouse X")*sensitivex, 0);
+	}*/
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (playerTransform != null) {
+			Vector3 newCameraPosition = playerTransform.position + _offset;
+			transform.position = Vector3.Lerp (transform.position, newCameraPosition, smoothing * Time.deltaTime);
+			//Rotate ();
+		}
+	}
+}
---
+++ Esercitazioni/Survival_Shooter - FPS/Assets/_CompletedAssets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform target;            // The position that that camera will be following.
+        public float smoothing = 5f;  // The speed with which the camera will be following.
+		public float sensitivex = 5f;
+
+
+        Vector3 offset;                     // The initial offset from the target.
+
+
+        void Start ()
+        {
+            // Calculate the initial offset.
+            offset = transform.position - target.position;
+        }
+
+		void Rotate()
+		{
+			transform.Rotate(0, Input.GetAxis("Mouse X")*sensitivex, 0);
+		}
+        void FixedUpdate ()
+        {
+            // Create a postion the camera is aiming for based on the offset from the target.
+            Vector3 targetCamPos = target.position + offset;
+
+            // Smoothly interpolate between the camera's current position and it's target position.
+            transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+			//Rotate ();
+        }
+    }
+}
